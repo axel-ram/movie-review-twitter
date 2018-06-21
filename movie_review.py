f8c526a10077cd379195f311379053608edb59a9
Ramnath Sharma <axel-ram@users.noreply.github.com>
2018-06-21 04:31:14 -0700

    Add files via upload

---
+++ movie_review.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+# importing libs
+import numpy as np
+import pandas as pd
+import re
+from nltk.stem.porter import PorterStemmer
+ps = PorterStemmer()
+
+from nltk.corpus import stopwords
+s = stopwords.words('English')
+temp=[]
+for word in s:
+	#removed all the stopwords having negative words
+    if not (word.endswith(('not', 'no', 'nor','n\'','n\'t','dn','an','sn','weren','tn','\''))) :
+        temp.append(word)
+
+#importing the dataset
+dataset = pd.read_csv('data.tsv', delimiter = "\t", quoting = 3)
+
+
+#cleaning dataset texts
+corpus = []
+for i in range(0,5000):
+    review = re.sub('[^a-xA-z]',' ',dataset['Phrase'][i])
+    review = review.lower()
+    review = review.split()
+    #stemming
+    review = [ps.stem(word) for word in review if not word in temp]
+    review = ' '.join(review)
+    corpus.append(review)
+
+#creating bag of words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=100)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:5000,3]
+
+# splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# fitting Random Forest Classification to the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+print('model trained')
+# dont need to predict here
+# predicting the Test set results
+#y_pred = classifier.predict(X_test)
+
+# output given below
+# making the Confusion Matrix
+'''
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+[[ 125  143   40    3    2]
+ [ 126  616  533   25    3]
+ [  42  353 3414  424   23]
+ [   0   41  633  802  160]
+ [   0    3   51  255  183]]
+
+
+efficiency
+0 - 0.75
+1 - 0.77
+2 - 0.87
+3 - 0.77
+4 - 0.76
+ '''
---
+++ r4.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+import movie_review as mr
+import collections
+import sys
+import  tweepy
+from textblob import  TextBlob
+
+# creating  consumer key and secret key 
+#  to authenticate twitter from ur accont 
+consumer_key='mPLp5SGMADa7HOKqtubHUTdZ2'
+
+consumer_secret='zZyoSpPgMCG5AznXXtlhivM6hW6cJ8ILVPnL7XcoLgy4IpC9Cz'
+
+#  creating  access key and secret key 
+
+access_key='1007186669244518400-6aFdNPYk6A2byYiUtLcQIsMLrTACAp'
+access_secret='T6oPrSC9aaU5s1BOsHpRNuFWRcyc30q4GhNwAMTzC4KB9'
+
+
+#  connecting for authenitcation 
+#  just like a sessional variable
+auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
+
+auth.set_access_token(access_key,access_secret)
+
+#  connecting to API  
+connect=tweepy.API(auth)
+
+review_list = []
+def recommend_sys(movie_name):
+	# serching for tweets to predict movie review and rating
+	get_data=connect.search(movie_name, count=500)
+	print("breakpoint 1")
+	review_list = []
+	for i  in  get_data:
+	    analysis=TextBlob(i.text)
+	    #ignoring nearly neutral polarity tweets
+	    #if (analysis.sentiment.polarity > 0.4 or analysis.sentiment.polarity < -0.4):
+	    review = mr.re.sub('[^a-xA-z]',' ',str(analysis))
+	    review = review.lower()
+	    review = review.split()
+	    #stemming
+	    review = [mr.ps.stem(word) for word in review if not word in mr.temp]
+	    review = ' '.join(review)
+	    review_list.append(review)
+	print("breakpoint 2")
+	#creating bag of words for training the model
+	from sklearn.feature_extraction.text import CountVectorizer
+	cv = CountVectorizer(max_features=100)
+	X = cv.fit_transform(review_list).toarray()
+	if(len(X[0]) < 100):
+		print("sorry this movie was not much reviewed and tweeted so system can not predict rating")
+		sys.exit()
+	# prediction from tweets
+	pred = mr.classifier.predict(X)
+	print('predicting rating')
+	#rating variables
+	zero=0
+	one=0
+	two=0
+	three=0
+	four=0
+	#determning rating
+	dict = collections.Counter(pred)
+	zero = dict[0] * 0.75
+	one = dict[1] * 0.77
+	two = dict[2] * 0.87
+	three = dict[3] * 0.77
+	four = dict[4] * 0.76
+	rating = max(zero,one,two,three,four)
+	print(rating/len(X))
+
+
+
+print('reciew system starting...')
+# taking input for movie name
+while(True):
+	movie_name = input("enter movie name")
+	if(movie_name == 'tixe'):
+		break
+	recommend_sys(movie_name)
